d40b7146b0c34aaad0620d4c2e9b8df2c0368ac7
Lambert D <lambertdem@hotmail.com>
2019-10-12 15:24:12 -0400

    debug

--- naive_bayes.py
+++ naive_bayes.py
@@ -24,11 +24,11 @@ def fit_naive_bayes(observations, y, num_features):
     #Marginal probability for each class
     marg_prob = np.true_divide(count_class, total_comments)
 
-    for i in range(len(observations)):
+    for i in range(observations.shape[0]):
         feature_no = observations[i][0]
         comment_no = observations[i][1]
        
-        comment_class = y[comment_no][1]
+        comment_class = y[comment_no]
         cond_prob_matrix[comment_class][feature_no] += 1
 
     #divide each row of cond_prob_matrix by the count of comments per class
