636de4e17c58d4a50eb10591ed1d5a38c5ad3fd4
Lambert D <lambertdem@hotmail.com>
2019-10-12 12:48:49 -0400

    fit function finished (not tested)

--- naive_bayes.py
+++ naive_bayes.py
@@ -5,22 +5,51 @@ import time
 
 
 
-def fit_naive_bayes():
+def fit_naive_bayes(observations, y, num_features):
 
     #Initialize marginal probability for each class
-    marg_prob = np.array(20*[[0]])
+    count_class = np.array(20*[[0]])
+    marg_prob = np.array(20*[[1]]) #Laplace smoothing, starting counts with 1
 
     #Initialize matrix of probabilities of observed features given k
-    num_features = 4
     cond_prob_matrix = np.empty((20,num_features))
 
-    print(marg_prob)
+    
+    #compute marginal probability of each class
+    total_comments = len(y)
+    for i in range(20):
+        for j in range(total_comments):
+            if y[j][1] == i:
+                count_class[i] += 1
+    
+    #Marginal probability for each class
+    marg_prob = np.true_divide(count_class, total_comments)
 
-    print(cond_prob_matrix)
+    for i in range(len(observations)):
+        feature_no = observations[i][0]
+        comment_no = observations[i][1]
+       
+        comment_class = y[comment_no][1]
+        cond_prob_matrix[comment_class][feature_no] += 1
+
+    #divide each row of cond_prob_matrix by the count of comments per class
+    for i in range(20):
+        cond_prob_matrix[i] = np.true_divide(cond_prob_matrix[i], count_class[i])
+
+    return marg_prob, cond_prob_matrix
+
+def predict(observations, marg_prob, cond_prob_matrix):
+
+    
 
 
 def main():
-    fit_naive_bayes()
+
+
+    marginal, conditional = fit_naive_bayes(observations, y, num_features)
+
+
+
 
 if __name__ == "__main__":
     main()
